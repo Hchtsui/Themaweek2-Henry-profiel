925d8bc06010afc5ed0b8c9bfdca1e8038f4868a
Hchtsui <112637278+Hchtsui@users.noreply.github.com>
2023-06-15 23:39:56 +0200

    thema week 2

---
+++ javascript/main.js
@@ -0,0 +1,27 @@
+const Texts = [
+    `who am i `, `Where am i good at`,
+    `Contact`, `Project` , `Skills`
+  
+  ];
+  
+  let tagCloud = TagCloud('.Sphere', Texts, {
+   
+      // Sphere radius in px
+      radius: 230,
+  
+      // animation speed
+      // slow, normal, fast
+      maxSpeed: 'normal',
+      initSpeed: 'fast',
+  
+      // Rolling direction [0 (top) , 90 (left), 135 (right-bottom)] 
+      direction: 135,
+  
+      // interaction with mouse or not [Default true (decelerate to rolling init speed, and keep rolling with mouse).]
+      keep: true
+  
+  });
+  
+  // Giving color to each text in sphere
+  let color = '#FFFFFF ';
+  document.querySelector('.Sphere').style.color = color;
